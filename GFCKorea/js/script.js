13754f95a16b8c5fc1625e323d7a7ccab0ca2c84
coolgayoung <gy_inbox@icloud.com>
2025-05-07 13:19:08 +0900

    자동 반영

---
+++ GFCKorea/js/script.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", function () {
+AOS.init();
+
+
+var swiper = new Swiper('.rollingBanner', {
+    loop: true, // Infinite loop of banners
+    // effect: 'fade',
+    autoplay: {
+        delay: 3000, // Change banner every 3 seconds
+    },
+    slidesPerView: 7,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination', // Optional: Add pagination dots
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // Optional: Next button
+        prevEl: '.swiper-button-prev', // Optional: Previous button
+    },
+    loopAdditionalSlides: 3,
+});
+
+var swiper = new Swiper('.rollingCardBanner', {
+    loop: true, // Infinite loop of banners
+    // effect: 'fade',
+    autoplay: {
+        delay: 3000, // Change banner every 3 seconds
+    },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination', // Optional: Add pagination dots
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // Optional: Next button
+        prevEl: '.swiper-button-prev', // Optional: Previous button
+    },
+    loopAdditionalSlides: 3,
+});
+
+$(document).ready(function () {
+    function startCounting() {
+        $('.counting').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+
+                //num 초기화
+                $this.text("0");
+
+            $({ countNum: 0 }).animate(
+                {
+                    countNum: countTo,
+                },
+                {
+                    duration: 1000,
+                    easing: 'linear',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                        console.log('finished: ${this.countNum}')
+                    },
+                }
+            );
+        });
+    }
+
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        return elemBottom > docViewTop && elemTop < docViewBottom;
+    }
+
+    var countingTriggered = false;
+
+    $(window).on('scroll', function () {
+        if (isScrolledIntoView('.grid-container')) {
+            if(!countingTriggered) {
+                console.log('숫자 카운팅 실행')
+                startCounting();
+                countingTriggered = true;
+
+                //3초 후 초기화, 재실행
+                setTimeout(() => {
+                    countingTriggered = false;
+                }, 3000);
+            }
+        }
+    });
+
+
+    
+});
+});
+
+
---
+++ GFCKorea/pie.vue
@@ -0,0 +1,31 @@
+
+new Vue({
+    el: '#app',
+    components: {
+        apexchart: VueApexCharts,
+    },
+    data: {
+        
+        series: [10, 10, 10, 60],
+        chartOptions: {
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        labels: ['아키텍처', '프로젝트 관리사', 'IT 컨설턴트', 'S/W개발 시스템운영'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+            }
+        }]
+        },
+    },
+
+})
+    
